03a0c63540f1cdeb482bbe6e7896c2d4a3defda5
Dominik Lewicki <dominik_03-06@wp.pl>
2024-03-18 22:40:28 +0100

    plpl

--- src/Components/O1.jsx
+++ src/Components/O1.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import './O1.css';
-
+import { HiMiniArrowDownRight } from "react-icons/hi2";
 const O1 = () => {
   return (
   <div className="o1">
+    <div className="absolut"></div>
     <div className="k1">
+<div className="left1">
+  <p className='big-txt1'>
+  The ultimate <span className='span-class'>getaway</span> to exceptional <span className='span-class'>user experience.</span>
+  <button className='btn1'>Start a project!
+  <HiMiniArrowDownRight />
+  </button>
+  </p>
+</div>
+<div className="right1">
 
+</div>
     </div>
   </div>
   )
 }
-
 export default O1
